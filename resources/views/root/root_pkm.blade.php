ed1df2adb950fd20ea4986dcffafe844b72793dc
sallieeky <sallieeky@gmail.com>
2022-01-23 10:28:18 +0800

    Refactor template pkm

--- resources/views/root/root_pkm.blade.php
+++ resources/views/root/root_pkm.blade.php
@@ -6,26 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield("title")</title>
+    <!-- Favicon -->
+    <link rel="icon" href="assets/img/brand/favicon.png" type="image/png">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+    <!-- Icons -->
+    <link rel="stylesheet" href="assets/vendor/nucleo/css/nucleo.css " type="text/css ">
+    <link rel="stylesheet " href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+    <!-- Page plugins -->
+    <!-- Argon CSS -->
+    <link rel="stylesheet" href="assets/css/argon.css?v=1.2.0 " type="text/css ">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/fontawesome.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
     <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-
-
-    <link rel="stylesheet" href=
-"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
-        integrity=
-"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
-        crossorigin="anonymous">
-  
-    <script src=
-        "https://code.jquery.com/jquery.min.js">
-    </script>
-    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
-
-
 </head>
 
 <body>
@@ -40,7 +35,7 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                     <form action="/logout">
-                        <button type="submit" class="rounded-pill btn-rounded border-primary">Logout
+                      <button type="submit" class="rounded-pill btn-rounded border-primary">Logout
                         <span>
                             <center><i class="fas fa-sign-out-alt"></i></center>
                         </span>
@@ -55,13 +50,12 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,96L48,128C96,160,192,224,288,213.3C384,203,480,117,576,117.3C672,117,768,203,864,202.7C960,203,1056,117,1152,117.3C1248,117,1344,203,1392,245.3L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
     </section>
 
-    @yield("content")
+      <!-- ////////////////////////////////////////////////////////////////////////////////////////
+                               START SECTION 2 - PKM IT
+/////////////////////////////////////////////////////////////////////////////////////////////-->
+  @yield("content")
 
-    <!-- BACK TO TOP BUTTON  -->
-    <a href="#" class="shadow btn-primary rounded-circle back-to-top">
-      <i class="fas fa-chevron-up"></i>
-  </a>
-
+  
   <script src="assets/vendors/js/glightbox.min.js"></script>
 
   <script type="text/javascript">
@@ -75,8 +69,6 @@
       });
   </script>
   <script src="assets/js/bootstrap.bundle.min.js"></script>
-
-  @yield("script")
-  
 </body>
+
 </html>
